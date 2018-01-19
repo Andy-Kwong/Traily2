723c512964414e626c9b13312bfa6a378979d1c5
Andy Kwong <andy.kwong@me.com>
2018-01-18 18:58:02 -0800

    Added GeoJSON Files

---
+++ Traily-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Traily-Bridging-Header.h
+//  Traily
+//
+//  Created by Andy Kwong on 1/18/18.
+//  Copyright © 2018 Andy Kwong. All rights reserved.
+//
+
+#ifndef Traily_Bridging_Header_h
+#define Traily_Bridging_Header_h
+#import <Pods/Google-Maps-iOS-Utils/GMUMarkerClustering.h>
+
+#endif /* Traily_Bridging_Header_h */
